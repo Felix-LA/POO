4e2fdfb4c54797749d9bddc838480ddba44b2a2d
Felix <felix.lobo85@gmail.com>
2025-10-25 10:23:39 -0400

    ....

---
+++ Atividade Kalebe/Att02.py
@@ -0,0 +1,13 @@
+class Aluno:
+    def __init__(self, nome, matricula):
+        self.matricula = matricula
+        self.nome = nome
+
+class Disciplina:
+    def __init__(self, nomeDisciplina, matricula):
+        self.nome = nomeDisciplina
+        self.matricula = matricula
+
+class nota:
+    def __init__(self, nota):
+        self.nota = nota
