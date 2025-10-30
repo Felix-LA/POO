34a0a3e2f3b00e6d4077984ee7932e4723b84ee2
Felix <felix.lobo85@gmail.com>
2025-10-30 16:43:40 -0400

    Fazendo Att03

--- Atividade Calebe/Att2/Att03.py
+++ Atividade Calebe/Att2/Att03.py
@@ -1,4 +1,6 @@
 class Att03:
+    alunos = []
+
     def __init__(self,Matricula,Nome,NotaProva1,NotaProva2,NotaProva3):
         self.matricula = Matricula
         self.nome = Nome
@@ -6,9 +8,40 @@ class Att03:
         self.notaProva2 = NotaProva2
         self.notaProva3 = NotaProva3
         self.media = (NotaProva1 + NotaProva2 + NotaProva3) / 3
-        alunos = []
 
-    def buscarAluno(self,nome):
+        Att03.alunos.append(self)
+
+
+
+    def buscarAluno(self,Nome):
+        if self.nome.lower() == Nome.lower():
+            return f"Aluno: {self.nome}\n Media: {self.media}"
+        else:
+            return f"Aluno {Nome} nao encontrado"
+        
+    
+    def verificarMedia(self,Nome):
+        if self.nome == Nome:
+            return self.media
+        else:
+            return f"Aluno {Nome} nao encontrado"
+        
+    def pegarMedia(self, aluno):
+        return aluno.media
+    
+
+    def maiorMedia(self):
+        maiorMedia = max(self.alunos, key= Att03.pegarMedia)
+        return maiorMedia
+    
+    def menorMedia(self):
+        menorMedia = min(self.alunos, key= Att03.pegarMedia)
+        return menorMedia
+    
+
+
+
+
         
 
     
