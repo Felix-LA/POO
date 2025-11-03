7190c415dbe4c5ba2ebdbad52871cbddae123424
Felix <felix.lobo85@gmail.com>
2025-11-03 18:47:43 -0400

    .....

--- Atividade Calebe/Att2/Att03.py
+++ Atividade Calebe/Att2/Att03.py
@@ -9,7 +9,7 @@ class Att03:
         self.notaProva3 = NotaProva3
         self.media = (NotaProva1 + NotaProva2 + NotaProva3) / 3
 
-        Att03.alunos.append(self)
+        # Att03.alunos.append(self.nome, self.media)
 
 
 
@@ -25,19 +25,45 @@ class Att03:
             return self.media
         else:
             return f"Aluno {Nome} nao encontrado"
-        
-    def pegarMedia(self, aluno):
-        return aluno.media
     
+    def aprovacao(self,Nome):
+        if self.media >= 6:
+            return f"Aluno Aprovado"
+        else:
+            return f"Aluno Reprovado"
 
-    def maiorMedia(self):
-        maiorMedia = max(self.alunos, key= Att03.pegarMedia)
-        return maiorMedia
-    
-    def menorMedia(self):
-        menorMedia = min(self.alunos, key= Att03.pegarMedia)
-        return menorMedia
-    
+
+
+def maiorMedia(alunos):
+    maiorMedia = 0
+    for i in alunos.values():
+        if i >= maiorMedia:
+            maiorMedia = i
+    return maiorMedia
+
+
+def menorMedia(alunos):
+    menorMedia = list(alunos.values())[0]
+    for i in alunos.values():
+        if i <= menorMedia:
+            menorMedia = i
+    return menorMedia
+
+
+# a1 = Att03(1, "Flka", 5, 7, 9)
+# a2 = Att03(2, "Pedro", 5, 6, 7)
+# b1 = Att03(3, "João", 7, 8, 9)
+# b2 = Att03(4, "Maria", 9, 9, 9)
+
+# a = [a1.media, a2.media, b1.media, b2.media]
+# lista_aluno_media = {}
+
+# for i in range(4):
+#     lista_aluno_media[i] = a[i]
+# buscarMaior = Att03.buscarAluno()
+# print("Maior média:", maiorMedia(lista_aluno_media))
+# print("Menor média:", menorMedia(lista_aluno_media))
+
 
 
 
---
+++ Atividade Calebe/Att2/Att05.py
@@ -0,0 +1,10 @@
+class Att05:
+    def __init__(self,Hora,Minuto,Segundo):
+        self.hora = Hora
+        self.minuto = Minuto
+        self.segundo = Segundo
+
+    def incrementoSegundo(self,Adicional):
+        self.segundo += Adicional
+        return 
+        
